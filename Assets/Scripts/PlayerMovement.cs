dcf0ebce9a880a005fcf1c7e6b82299e458bb262
Artem Rozhkov <artem.rozhkov.1996@gmail.com>
2024-02-01 02:07:56 +0200

    Implement player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,21 +7,61 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float runSpeed = 7f;
+    [SerializeField] float jumpSpeed = 5f;
+
     Vector2 moveInput;
+    Rigidbody2D playerRigidbody;
+    Animator playerAnimator;
+    CapsuleCollider2D playerCapsuleCollider;
 
     void Start()
     {
-
+        playerRigidbody = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponent<Animator>();
+        playerCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
     {
-
+        Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log(moveInput);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!playerCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) return;
+
+        if (value.isPressed)
+        {
+            playerRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
+
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, playerRigidbody.velocity.y);
+        playerRigidbody.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
+        playerAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(playerRigidbody.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(MathF.Sign(playerRigidbody.velocity.x), 1f);
+        }
+
     }
 }
